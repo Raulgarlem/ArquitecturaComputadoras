3f179508e538c6e380b52c2b644e320b8effcfe1
EduardoBarcoNunez <42382920+EduardoBarcoNunez@users.noreply.github.com>
2025-11-08 12:07:45 -0600

    añadi instrucciones

--- Practica6/rom_p6.vhd
+++ Practica6/rom_p6.vhd
@@ -9,15 +9,68 @@ entity rom_p6 is
   );
 end rom_p6;
 
-architecture rtl of rom_p6 is
-  -- 256 bytes
-  type rom_t is array (0 to 255) of std_logic_vector(7 downto 0);
-  signal ROM : rom_t := (others => (others => '0'));
-
-  -- dile a Quartus que lo llene con tu .hex
-  attribute ram_init_file : string;
-  attribute ram_init_file of ROM : signal is "programa68hc11.hex";
+architecture Behavioral of rom_p6 is
 begin
-  -- usamos solo los 8 bits bajos de la dirección
-  datao <= unsigned(ROM(to_integer(addr(7 downto 0))));
-end rtl;
+  process(addr)
+  begin
+    case addr is
+      -- LDAA #$FF ? estados 860, 861, 862
+      when x"0014" => datao <= x"86";
+      when x"0015" => datao <= x"FF";
+
+      -- LDAB #$01 ? estados C60, C61, C62
+      when x"0016" => datao <= x"C6";
+      when x"0017" => datao <= x"01";
+
+      -- LDX #$0010 ? estados CE0, CE1, CE2
+      when x"0018" => datao <= x"CE";
+      when x"0019" => datao <= x"00";
+      when x"001A" => datao <= x"10";
+
+      -- ABA ? estado 1B0
+      when x"001B" => datao <= x"1B";
+
+      -- BNE ET1 ? estados 260, 261, 262, 263
+      when x"001C" => datao <= x"26";
+      when x"001D" => datao <= x"02"; -- salto relativo (+2)
+
+      -- STAA 0,X ? estados A700, A701
+      when x"001E" => datao <= x"A7";
+      when x"001F" => datao <= x"00";
+
+      -- BRA ET2 ? estados 200, 201
+      when x"0020" => datao <= x"20";
+      when x"0021" => datao <= x"02";
+
+      -- ET1: STAB 0,X ? estados E700, E701
+      when x"0022" => datao <= x"E7";
+      when x"0023" => datao <= x"00";
+
+      -- ET2: LDAA #$07 ? estados 860, 861, 862
+      when x"0024" => datao <= x"86";
+      when x"0025" => datao <= x"07";
+
+      -- LDAB #$02 ? estados C60, C61, C62
+      when x"0026" => datao <= x"C6";
+      when x"0027" => datao <= x"02";
+
+      -- MUL ? estado 3D0
+      when x"0028" => datao <= x"3D";
+
+      -- STAA 1,X ? estados A701, A702
+      when x"0029" => datao <= x"A7";
+      when x"002A" => datao <= x"01";
+
+      -- STAB 2,X ? estados E702, E703
+      when x"002B" => datao <= x"E7";
+      when x"002C" => datao <= x"02";
+
+      -- FIN: BRA FIN ? estados 200, 201
+      when x"002D" => datao <= x"20";
+      when x"002E" => datao <= x"FE"; -- salto relativo (-2)
+
+      when others => datao <= x"00";
+    end case;
+  end process;
+end Behavioral;
+
